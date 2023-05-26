ca8f5f02d01cd71e75b1e47e2bff739b3b04c6e9
Yash Sathwara <69532073+darkwarrior303@users.noreply.github.com>
2023-05-26 11:05:50 +0000

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,53 +1,63 @@
-import { AppBar, Box, Button, CssBaseline, Toolbar, Typography } from '@mui/material'
-import React from 'react'
-import { Link } from 'react-router-dom';
+import {
+  AppBar,
+  Box,
+  Button,
+  CssBaseline,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-    const navItems = [
-        {
-            name: "Home",
-            path: "/"
-        },
-        {
-            name: "create-player",
-            path: "/create-player"
-        },
-        {
-            name: "create-team",
-            path: "/create-team"
-        },
-        {
-            name: "create-match",
-            path: "/create-match"
-        }
-    ];
+  const navItems = [
+    {
+      name: "Home",
+      path: "/",
+    },
+    {
+      name: "create-player",
+      path: "/create-player",
+    },
+    {
+      name: "create-team",
+      path: "/create-team",
+    },
+    {
+      name: "create-match",
+      path: "/create-match",
+    },
+  ];
 
-    return (
-        <Box sx={{ display: 'flex' }}>
-            <CssBaseline />
-            <AppBar component="nav">
-                <Toolbar>
-                    <Typography
-                        variant="h6"
-                        component="div"
-                        sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
-                    >
-                        Cricket
-                    </Typography>
-                    <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
-                        {navItems.map((item, i) => (
-                            <Button key={i} sx={{ color: '#fff' }}>
-                                <Link to={item.path}>
-                                    {item.name}
-                                </Link>
-                            </Button>
-                        ))}
-                    </Box>
-                </Toolbar>
-            </AppBar>
-            <Toolbar />
-        </Box>
-    )
-}
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar component="nav">
+        <Toolbar>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+          >
+            Cricket
+          </Typography>
+          <Box sx={{ display: { xs: "none", sm: "block" } }}>
+            {navItems.map((item, i) => (
+              <Button key={i} sx={{ color: "#fff" }}>
+                <Link
+                  to={item.path}
+                  style={{ color: "#fff", textDecoration: "none" }}
+                >
+                  {item.name}
+                </Link>
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Toolbar />
+    </Box>
+  );
+};
 
-export default Header
+export default Header;
